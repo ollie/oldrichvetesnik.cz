20cda4342655df5dca2357dcc723890a16e30b89
Oldrich Vetesnik <mrmil@igloonet.cz>
2012-10-25 11:31:17 +0200

    Added CV, refactored a bit

--- app.rb
+++ app.rb
@@ -1,15 +1,12 @@
-require 'bundler/setup'
+require 'bundler'
+Bundler.require
 
-require 'sinatra'
-require 'haml'
-require 'httparty'
 require 'json'
-# require 'pry'
 require './lib/partials'
 require './lib/core_ext'
 
-set :protection, :except => :frame_options # Turn off the X-Frame-Options header to be able to display iframe contents
-set :haml, :format => :html5
+set :protection, except: :frame_options # Turn off the X-Frame-Options header to be able to display iframe contents
+set :haml, format: :html5, layout: :'layouts/application'
 
 helpers do
   include Sinatra::Partials
@@ -19,13 +16,13 @@ def current_env
   ENV['RACK_ENV']
 end
 
-# def basic_haml(template, options = {})
-#   haml template, options.merge(:layout => :'basic_layout')
-# end
-
 get '/' do
   @year = Time.now.year
   haml :index
+end
+
+get '/cv' do
+  haml :cv, layout: :'layouts/cv'
 end
 
 get '/github-repositories' do
@@ -39,5 +36,5 @@ get '/github-repositories' do
     tmp
   end.sort! { |repo_a, repo_b| repo_b[:pushed_at] <=> repo_a[:pushed_at] }.slice_in(3)
 
-  haml :github_repositories, :layout => false
+  haml :github_repositories, layout: false
 end
--- lib/partials.rb
+++ lib/partials.rb
@@ -4,12 +4,12 @@ module Sinatra
       template_array = template.to_s.split('/')
       template = template_array[0..-2].join('/') + "/_#{template_array[-1]}"
       options = args.last.is_a?(Hash) ? args.pop : {}
-      options.merge!(:layout => false)
+      options.merge!(layout: false)
       locals = options[:locals].nil? ? {} : options[:locals] # SAVE LOCALS
       if collection = options.delete(:collection) then
         collection.inject([]) do |buffer, member|
-          buffer << haml(:"#{template}", options.merge(:layout =>
-          false, :locals => {template_array[-1].to_sym => member}.merge(locals))) # MERGE THEM BACK TO EACH
+          buffer << haml(:"#{template}", options.merge(
+            layout: false, locals: {template_array[-1].to_sym => member}.merge(locals))) # MERGE THEM BACK TO EACH
         end.join("\n")
       else
         haml(:"#{template}", options)
